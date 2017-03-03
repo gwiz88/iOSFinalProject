07d52ad92e9fd3d3d17e3fd9e0c2445aa3e45504
cpmarci <cmarcill@macintosh-4.cstcis.cti.depaul.edu>
2017-03-03 12:37:43 -0600

    scanner added

---
+++ iOSFinalProject/TheActualScannerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TheActualScannerViewController.swift
+//  iOSFinalProject
+//
+//  Created by Marcille, Charles on 3/3/17.
+//  Copyright © 2017 Gwizdalski, Marcille, Wong. All rights reserved.
+//
+
+import UIKit
+
+class TheActualScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
