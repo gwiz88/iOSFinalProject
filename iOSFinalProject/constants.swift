5105dcef355bfd0b5610c0fb21a5fdd729e5b65d
cpmarci <philip.marcille@gmail.com>
2017-03-11 15:25:53 -0600

    constants file added.  api id, key, and url

---
+++ iOSFinalProject/constants.swift
@@ -0,0 +1,16 @@
+//
+//  constants.swift
+//  iOSFinalProject
+//
+//  Created by user on 3/11/17.
+//  Copyright © 2017 Gwizdalski, Marcille, Wong. All rights reserved.
+//
+
+import Foundation
+
+let API_AUTH_URL = "https://api.nutritionix.com/v1_1/item?upc="
+
+let API_ID = "95905eed"
+
+let API_KEY = "41d00f33c7ee550371a45440f8064968"
+
